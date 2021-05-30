036bc99444ef0d594e71077e330374cb4a6c5dd5
Anh Khoa <binm8.4356@gmail.com>
2021-05-30 17:30:26 +0700

    Thêm giá vào title

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,5 +1,4 @@
-@extends('layouts.app')
-@section('title', ['title' => 'Acc'. $account->game->sort_name . '#' . $account->id . ' | Giá: ' . number_format($account->price) . 'đ'])
+@extends('layouts.app', ['title' => 'Acc'. $account->game->sort_name . '#' . $account->id . ' | Giá: ' . number_format($account->price) . 'đ'])
 @section('content')
 <div class="sl-dtprod">
     <div class="container">
